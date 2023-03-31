65480ac1bc2f414788c741930c85ce9a3c4207ff
Bryan Villamil <bryan.villamil@BRYANVILLAMIL.local>
2023-03-31 10:53:44 -0500

    general chances

--- src/hooks/useElementOnViewport.js
+++ src/hooks/useElementOnViewport.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 export const useElementOnViewport = (targetRef) => {
     

d54938bde9fc19c0f07f562ba19bd47a9c71a963
Bryan Villamil <bryan.villamil@BRYANVILLAMIL.local>
2023-04-04 18:42:46 -0500

    styled in btn wp mobile

--- src/components/atoms/BtnWhatsapp/index.tsx
+++ src/components/atoms/BtnWhatsapp/index.tsx
@@ -6,7 +6,7 @@ export const BtnWhatsapp = () => {
   const number = '3116448934'
   return (
     <ContainerLinkWp
-      href={`https://api.whatsapp.com/send?phone=+${number}&text=Bienvenidos%20a%20IENEL,%20escribemos%20y%20danos%20el%20placer%20de%20atenderte`}
+      href={`https://api.whatsapp.com/send?phone=+${number}&text=Bienvenidos%20a%20IENEL,%20es%20un%20placer%20atenderte,%20cuentanos%20como%20podemos%20ayudarte`}
       target="_blank">
       <BsWhatsapp color="#fff" />
     </ContainerLinkWp>

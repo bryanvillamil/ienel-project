a207d219a1dbf1e10f4d4180eba8d937f31a7b8c
jhordanv10 <jhordanvilla1000@gmail.com>
2023-04-17 11:36:56 -0500

    Changes in section services

--- src/components/atoms/CardService/index.tsx
+++ src/components/atoms/CardService/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Paragraph } from '@components/index'
+import { Paragraph, Title } from '@components/index'
 import {
   Card,
   Container,
@@ -32,26 +32,31 @@ export const CardService: React.FC<ServicesProps> = ({ service }) => {
           onMouseLeave={handleCardFlipLeave}>
           <CardFront
             style={{ transform: isFlipped ? 'rotateY(180deg)' : 'none' }}>
-            <service.iconoDelServicio size={120} />
+            <service.iconoDelServicio size={60} />
           </CardFront>
           <CardBack
             style={{ transform: isFlipped ? 'none' : 'rotateY(180deg)' }}>
             <Paragraph
               text={service.descripcionDelServicio}
               size={14}
-              color="white"
+              color="black"
               align="center"
             />
           </CardBack>
         </CardContainer>
       </Card>
       <Text style={{ marginTop: '15px' }}>
-        <Paragraph
+        {/* <Paragraph
           text={service.nombreDelServicio}
           size={16}
           color=""
           align="center"
-        />
+        /> */}
+        <Title
+          type={6}
+          text={service.nombreDelServicio}
+          color=""
+          align="center"></Title>
       </Text>
     </Container>
   )
--- src/components/atoms/CardService/styledComponents.js
+++ src/components/atoms/CardService/styledComponents.js
@@ -14,23 +14,23 @@ export const CardFront = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #f1f1f1;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  border-radius: 10px;
+  /* background-color: #f1f1f1; */
+  /* box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; */
+  /* border-radius: 10px; */
   position: absolute;
   width: calc(100%);
   height: calc(100%);
   backface-visibility: hidden;
-  background-color: #f1f1f1;
+  /* background-color: #f1f1f1; */
   transform: ${(props) => (props.flipped ? 'rotateY(180deg)' : 'none')};
   transition: transform 0.6s ease;
-  color: ${(props) => props.theme.colors.colorPrimary};
+  /* color: ${(props) => props.theme.colors.colorPrimary}; */
 `
 
 export const CardBack = styled.div`
-  background-color: ${(props) => props.theme.colors.colorPrimary};
+  /* background-color: ${(props) => props.theme.colors.colorPrimary}; */
   transform: rotateY(180deg);
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  /* box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; */
   border-radius: 10px;
   display: flex;
   justify-content: center;
@@ -44,6 +44,7 @@ export const CardBack = styled.div`
   p {
     max-width: 85%;
   }
+  color: red;
 `
 export const Card = styled.div`
   width: 180px;
@@ -71,10 +72,15 @@ export const Text = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-  p {
-    max-width: 85%;
-    font-weight: 700;
+  h6 {
+    max-width: 50%;
+    margin: 0 auto;
+    border-top: 3px solid black;
+    font-weight: 500;
     color: black;
+    font-size: 16px;
+    text-transform: uppercase;
+    letter-spacing: 0.1rem;
   }
 `
 
